4baea9ffbe61bc7fd1640951e5cfc4be5f9e04be
jubayerkhan <jubayerkhan741@gmail.com>
2024-10-19 13:44:56 +0600

    server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors')
+const multer = require('multer');
+const path = require('path');
 const mongoose = require('mongoose');
 const Product = require('./models/productModel')
 const app = express();
@@ -7,6 +9,33 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
+
+
+// upload image
+const storage = multer.diskStorage({
+    destination: './uploads/', // Define the directory to store images
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1000000 }, // Limit file size to 1MB
+    fileFilter: (req, file, cb) => {
+        const filetypes = /jpeg|jpg|png|gif/;
+        const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+        const mimetype = filetypes.test(file.mimetype);
+        if (mimetype && extname) {
+            return cb(null, true);
+        } else {
+            cb('Error: Images Only!');
+        }
+    }
+}).single('image');
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 //routes
 
@@ -38,13 +67,25 @@ app.get('/products/:id', async (req, res) => {
 })
 
 app.post('/products', async (req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).json({ message: error.message })
-    }
+    upload(req, res, async (error) => {
+        if (error) {
+            res.status(400).json({message: error})
+        }
+
+        try {
+            const product = new Product({
+                ...req.body,
+                image: req.file ? `http://localhost:3000/` + req.file.path.replace(/\\/g, '/') : ''
+            })
+
+            await product.save()
+
+            res.status(200).json(product);
+        } catch (error) {
+            console.log(error.message)
+            res.status(500).json({ message: error.message })
+        }
+    })
 })
 
 //update a product
